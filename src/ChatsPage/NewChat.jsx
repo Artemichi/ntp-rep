673721ae2f1c654def43b221a1d0d8ef0f7942c0
Artemichi <55092560+Artemichi@users.noreply.github.com>
2020-04-22 04:48:20 +0500

    deleted trash

--- src/ChatsPage/NewChat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import firebase from 'firebase/app'
-import { FormControl, InputLabel, Input, Button, Paper, withStyles, CssBaseline, Typography } from '@material-ui/core'
-
-const NewChat = () => {
-  return <div></div>
-}
-
-export default NewChat
